a0aa7cbb1a71e47b89eeb9e078fc56aaa54e2fd0
ShravaniR2412 <shravanirasam0212@gmail.com>
2024-03-27 01:02:02 +0530

    workshop pg details.js

--- src/assets/workshops/details.js
+++ src/assets/workshops/details.js
@@ -5,7 +5,9 @@ const details=
         "subtitle": "UI-UX Workshop",
         "date": "13 September 2023",
         "image_path": Uiux,
-        "description": "Participants were taught about basic ui-ux components along with hands on experience. There were more than 80 participants."
+        "description": "On September 12th and 13th, 2023, from 3:00 PM to 5:00 PM, Room B52 buzzed with creativity and innovation as participants gathered for a transformative workshop on User Interface (UI) and User Experience (UX) design. Through interactive lectures, hands-on exercises, and real-world case studies, participants delved deep into the intricacies of designing intuitive and user-friendly interfaces. They learned how to conduct user research, create wireframes and prototypes, and apply best practices for usability and accessibility.",
+        "Participants": "There were more than 80 participants."
+
     },
     
     {
@@ -13,7 +15,9 @@ const details=
         "subtitle": "Career Guidance",
         "date": " 4th October 2023",
         "image_path": ByteSize,
-        "description": "Talk about how we can find our feet and do well in a world so fast-moving and vast."
+        "description": "On October 4th, 2023, from 3:00 PM to 5:00 PM, the Auditorium became a beacon of guidance and inspiration as participants gathered for an enlightening session on navigating the fast-paced world and finding their footing amidst its vastness. Led by esteemed speaker Mr. Faiz Malkani, Mentor at Google for Startups, GDG MAD organizer, and Google Expert for Product Design, the event promised insights, strategies, and actionable advice to help participants thrive in an ever-evolving landscape.As Mr. Faiz Malkani took the stage, the atmosphere crackled with anticipation. Drawing upon his wealth of experience and expertise, he illuminated the path forward for participants, offering practical guidance on how to succeed in a world characterized by rapid change and boundless opportunities.",
+        "Participants":"8 participants attended the event."
+
     },
    
     {
@@ -21,21 +25,25 @@ const details=
         "subtitle": "UI-UX Competition",
         "date": "9th October 2023",
         "image_path": Uiuxcomp,
-        "description": "UI/UX COMPETITION in collaboration with Friends of Figma and Riddl.Design an app on Figma to help balance work and personal life."
+        "description": "UI/UX COMPETITION in collaboration with Friends of Figma and Riddl.Design an app on Figma to help balance work and personal life.",
+        "Participants": "There were more than 10 participants."
     },
     {
         "title": "Jarvis",
         "subtitle": "Natural Language Processing",
         "date": "21st & 22nd December 2023",
         "image_path": Jarvis,
-        "description": "Workshop on Natural Language Processing where participants made their own J.A.R.V.I.S., an A.I.-based desktop assistant.There were 60 participants."
+        "description": "The two-day workshop on 'Building Your Own J.A.R.V.I.S. - An A.I.-Based Desktop Assistant' was a resounding success, attracting enthusiastic participants eager to delve into the world of Natural Language Processing (NLP) and artificial intelligence. Hosted on Google Meet, the workshop provided a comprehensive introduction to NLP principles and hands-on experience in developing a personalized desktop assistant reminiscent of the iconic J.A.R.V.I.S. from the Marvel universe.Participants gained insights into the various applications of NLP and its role in powering intelligent virtual assistants like J.A.R.V.I.S. The session also covered an overview of J.A.R.V.I.S. architecture, highlighting key components such as speech recognition, natural language understanding, and task execution.",
+        "Participants": "There were 60 participants."
+
     },
     {
         "title": "Dev Hub India",
         "subtitle": "Web3 Fundamentals",
         "date": "24th January 2024",
         "image_path": DevHub,
-        "description": "Web3 wallets, Cairo, and essentials of OnlyDust and Rust were covered.70 participants were given Food, Goodies and Stickers."
+        "description": "The event on Web3 wallets, Cairo, and the essentials of OnlyDust and Rust was not only informative but also rewarding for its attendees. With a turnout of 70 participants, each individual was provided with food, goodies, and stickers to enhance their experience and make the event memorable. As they delved into the complexities of Web3 wallets and explored the intricacies of programming languages like Cairo, OnlyDust, and Rust, attendees were able to enjoy delicious refreshments, collect exciting goodies, and proudly display their event stickers. This thoughtful gesture added an extra layer of enjoyment to the learning process, fostering a sense of community and appreciation among all participants.",
+        "Participants": "70 participants"
     },
     
     {
@@ -43,7 +51,8 @@ const details=
         "subtitle": "Tableau Workshop",
         "date": " 12th and 13th February 2024",
         "image_path": Tableau,
-        "description": "In this workshop, participants were taught the basics of Tableau and how to create a dashboard using a large dataset(Netflix users' data) and graphing using different parameters."
+        "description": "The Tableau workshop held on February 12th and 13th, 2024, in room B31, offered participants a practical introduction to data visualization. Attendees learned to navigate Tableau's interface, connect to datasets, and create interactive dashboards using a large dataset of Netflix users' data. Through hands-on exercises, they explored various graphing techniques and gained valuable skills to analyze and present data effectively.",
+        "Participants": "19 students participated in this event."
     },
    
     {
@@ -51,7 +60,9 @@ const details=
         "subtitle": "AdvanceExcel Workshop",
         "date": "21st and 22nd February 2024",
         "image_path": AdvExcel,
-        "description": "Basic commands and uses of Excel along with hands on  project which was created using advanced commands of Excel to create a dashboard."
+        "description":"The Excel workshop conducted on February 21st and 22nd, 2024, in room B32, provided participants with a comprehensive learning experience. On the first day, attendees were introduced to basic commands and functionalities of Excel, laying a strong foundation. The following day, participants applied advanced Excel commands to create a project, specifically focusing on dashboard creation. Through this hands-on approach, participants gained practical skills and knowledge to efficiently utilize Excel for data management and analysis.",
+        "Participants": "11 students participated in this event."   
+
     }
 ]
 
